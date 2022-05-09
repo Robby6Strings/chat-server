7243c33ab81af8e0b15937ff76bd8ff06700f107
Robby6Strings <rausten93@gmail.com>
2022-05-09 21:05:16 +1200

    added user name update

--- sockets.js
+++ sockets.js
@@ -395,8 +395,7 @@ function addClient(socket, user) {
   let { name, id } = user;
 
   socket.__clientId = id || crypto.randomUUID();
-  console.log('addClient', socket.__clientId);
-  clients.set(id, {
+  clients.set(socket.__clientId, {
     name,
     selectedChannelId: null,
     socket,
